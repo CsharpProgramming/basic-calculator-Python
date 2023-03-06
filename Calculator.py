57a33ce19a1718d5be196daeeca5600a71299073
C# <chashtag.code@gmail.com>
2023-03-06 07:25:39 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,21 @@
 print("Welcome to my Calculator")
 
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 method = input("Enter operation name (+, -, *, /): ")
 
 if method == "+":
-    print(int(num1) + int(num2))
+    print(num1 + num2)
 
 elif method == "-":
-    print(int(num1) - int(num2))
+    print(num1 - num2)
 
 elif method == "*":
-    print(int(num1) * int(num2))
+    print(num1 * num2)
 
 elif method == "/":
-    print(int(num1) / int(num2))
+    print(num1 / num2)
 
 else:
     print("Wrong operation name")
