82c25fa6e94c9486641473bcbcab787a2dbaeaca
C# <chashtag.code@gmail.com>
2023-03-06 18:51:28 +0100

    Change int to float

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 print("Welcome to my Calculator")
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 method = input("Enter operation name (+, -, *, /): ")
 
